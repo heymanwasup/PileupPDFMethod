d5ca83bb300d085eaa01643c5ed00669bf955c09
Cheng Chen <cheng.chen@cern.ch>
2022-05-12 00:47:56 +0800

    typo fix

--- run_local_triple.sh
+++ run_local_triple.sh
@@ -1,13 +1,3 @@
 #! /bin/bash
-source  /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
-
-STATUS=write
-EXE=PileupConstructer.exe
-
-inputFile=/home/chencheng/data/Run4U/oldfitter/hists.root
-# inputFile=/home/chencheng/data/Run4U/oldfitter/hists_PU_All.root
-outputFile=/home/chencheng/data/Run4U/oldfitter/hists_PU_triple_func_All_final_test.root
-# mkdir -p ${outputDir}
-# g++ `root-config --cflags --glibs` -o ${EXE}.exe PileupConstructer.cpp &&\
-./${EXE}.exe -i ${inputFile} -o ${outputFile} -s ${STATUS}
-echo -e "Finished"
+g++ `root-config --cflags --glibs` -o PileupConstructer.exe PileupConstructer.cpp \
+&& ./run_condor.sh Newfitter All 1
